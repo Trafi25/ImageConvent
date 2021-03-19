75e3ac9fb4aa13c4c09d86ab32b02ce242074b1c
Trafi25 <45690086+Trafi25@users.noreply.github.com>
2021-03-19 11:50:33 +0200

    InitialCommit

---
+++ Conventor/ImageConcept/ImageCons.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conventor.ImageConcept
+{
+    class ImageCons
+    {
+    }
+}
---
+++ Conventor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conventor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
