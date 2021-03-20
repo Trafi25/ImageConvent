689963ccfe9c9d0f6b253feeae8465e0e5da014c
Trafi25 <45690086+Trafi25@users.noreply.github.com>
2021-03-20 15:33:26 +0200

    better Reader

--- Conventor/Reader/ReadBMP.cs
+++ Conventor/Reader/ReadBMP.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Conventor.ImageConcept;
 using Conventor.Interfaces;
@@ -8,8 +9,44 @@ using Conventor.Interfaces;
 
 namespace Conventor.Reader
 {
-    class ReadBMP 
+    class ReadBMP : IImageReader
     {
-        
+        byte[] fileData;
+        internal byte[] ReadFile(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open);
+            fileData = new byte[fs.Length];
+            fs.Read(fileData, 0, fileData.Length);
+            fs.Close();
+            return fileData;
+        }
+
+        internal void BMPGetHeader()
+        {
+            // initializing the array size of 14 bytes for the BMP header
+
+            byte[] headerBMP = new byte[14];
+
+            for (int i = 0; i < headerBMP.Length; i++)
+            {
+                headerBMP[i] = fileData[i];
+            }
+        }
+
+        public Image Read(string path)
+        {
+            try
+            {
+                ReadBMP objDataInfo = new ReadBMP();
+                objDataInfo.ReadFile(path);
+                objDataInfo.BMPGetHeader();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc);
+            }
+
+            throw new NotImplementedException();
+        }
     }
 }
