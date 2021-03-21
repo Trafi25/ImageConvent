27fa4500cf42fcc650d2ea882af999e5b73563ac
MrKrip <45828333+MrKrip@users.noreply.github.com>
2021-03-21 16:00:42 +0200

    Вроде все маркеры считал

--- Conventor/Reader/ReadJPEG.cs
+++ Conventor/Reader/ReadJPEG.cs
@@ -57,9 +57,21 @@ namespace Conventor.Reader
                             temp = string.Empty;
                         }
                        break;
+                    case "ffe0":
+                        {
+                            hex = CommentSection(hex);
+                            temp = string.Empty;
+                        }
+                        break;
                    case "ffdb":
                         {
                             hex = QuantizationMatrix(hex);
+                            temp = string.Empty;
+                        }
+                        break;
+                    case "ffc0":
+                        {
+                            hex = BaselineDCT(hex);
                             temp = string.Empty;
                         }
                         break;
@@ -69,6 +81,14 @@ namespace Conventor.Reader
                             temp = string.Empty;
                         }
                         break;
+                    case "ffda":
+                        {
+                            hex = StartOfScan(hex);
+                            temp = string.Empty;
+                        }
+                        break;
+                    case "ffd9":
+                        return;
                }                
             }
         }
@@ -82,6 +102,7 @@ namespace Conventor.Reader
 
         private string QuantizationMatrix(string hex)
         {
+            Console.WriteLine();
             string sizestr = hex.Substring(0, 4);
             int size = int.Parse(sizestr, System.Globalization.NumberStyles.HexNumber);
             Console.WriteLine($"Size of Quantiztion:{size}");
@@ -95,8 +116,28 @@ namespace Conventor.Reader
             return hex;
         }
 
+        public string BaselineDCT(string hex)
+        {
+            Console.WriteLine();
+            string sizestr = hex.Substring(0, 4);
+            int size = int.Parse(sizestr, System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine($"Size of Quantiztion:{size}");
+            int Precision = int.Parse(hex.Substring(4,2));
+            Console.WriteLine($"Precision:{Precision}");
+            int heigth = int.Parse(hex.Substring(6,4),System.Globalization.NumberStyles.HexNumber);
+            int width = int.Parse(hex.Substring(10, 4), System.Globalization.NumberStyles.HexNumber);
+            int NumberOfChannels = int.Parse(hex.Substring(14, 2), System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine($"heigth:{heigth}");
+            Console.WriteLine($"width:{width}");
+            Console.WriteLine($"NumberOfChannels:{NumberOfChannels}");
+            //add here foreach NumberOfChannels
+            hex = hex.Substring(size * 2);
+            return hex;
+        }
+
         private string DHTSection(string hex)
         {
+            Console.WriteLine();
             string sizestr = hex.Substring(0, 4);
             int size = int.Parse(sizestr, System.Globalization.NumberStyles.HexNumber);
             Console.WriteLine($"Size of DHTSection:{size}");
@@ -107,6 +148,21 @@ namespace Conventor.Reader
             Console.WriteLine($"TableId:{TableId}");
             Console.WriteLine($"DHTSection:{DHTSection}");
             hex = hex.Substring(size * 2);
+            return hex;
+        }
+
+        private string StartOfScan(string hex)
+        {
+            Console.WriteLine();
+            string sizestr = hex.Substring(0, 4);
+            int size = int.Parse(sizestr, System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine($"Size of StartOfScan:{size}");
+            int NumberOfChannels = int.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+            //add here foreach NumberOfChannels
+            hex = hex.Substring(size * 2);
+            string EncodedData = hex.Substring(0,hex.Length-4);
+            Console.WriteLine($"EncodedData:{EncodedData}");
+            hex = hex.Substring(hex.Length - 4);
             return hex;
         }
 
