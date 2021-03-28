51a831ecca199e0ef248edd0127e3abe0184d039
MrKrip <45828333+MrKrip@users.noreply.github.com>
2021-03-28 16:10:28 +0300

    Add exception class

---
+++ Conventor/Exceptions/FormatSupportedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conventor.Exeptions
+{
+    class FormatSupportedException:Exception
+    {
+        public string ErrorDetails; 
+        FormatSupportedException(string message,string format):base(message)
+        {
+            ErrorDetails = $"{message} : {format}";
+        }
+    }
+}
